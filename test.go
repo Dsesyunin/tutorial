70f70f1138f968bd93ff99fb913599a21d8580e3
Dima <44135905+Korol-Kazinak@users.noreply.github.com>
2023-09-21 20:15:27 +0300

    add new message

--- test.go
+++ test.go
@@ -6,6 +6,5 @@ func main() {
 	fmt.Println("Hello world!")
 	fmt.Println("Hello class")
 	fmt.Println("Hello people")
-	fmt.Println("Goodnight")
-	fmt.Println("task-1 message")
+	fmt.Println("new message")
 }
